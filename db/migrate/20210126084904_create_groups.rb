744567f770a096c93f9cd497487509d6f08dd570
angel <albarros1973@gmail.com>
2021-01-26 09:56:08 +0100

    Add Group model

---
+++ db/migrate/20210126084904_create_groups.rb
@@ -0,0 +1,9 @@
+class CreateGroups < ActiveRecord::Migration[6.1]
+  def change
+    create_table :groups do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
