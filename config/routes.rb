7deaf8b77600f6f9182a26896a933473e10e47c0
angel <albarros1973@gmail.com>
2021-01-26 16:06:20 +0100

    Add log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
 end
