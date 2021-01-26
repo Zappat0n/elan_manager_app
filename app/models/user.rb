c9f6c78eda3eccf63d05a2310f475724d8ae7ac0
angel <albarros1973@gmail.com>
2021-01-26 16:14:13 +0100

    Remove email index from users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :presentations
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, authentication_keys: [:name]
+         :rememberable, authentication_keys: [:name]
 
   # validates :email, uniqueness: true
   validates :name, uniqueness: true, presence: true, length: { minimum: 3 }
---
+++ db/migrate/20210126151200_remove_email_index_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveEmailIndexFromUsers < ActiveRecord::Migration[6.1]
+  def change
+    remove_index :users, :email
+  end
+end
