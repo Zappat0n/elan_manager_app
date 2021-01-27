506ad030adb2a0350813787f513de391b3dfa1cf
angel <albarros1973@gmail.com>
2021-01-27 11:54:03 +0100

    Update user view partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     current_user.avatar = params[:avatar]
     current_user.sava
   end
+
+  def current_avatar
+    unless current_user.avatar.attached?
+      image = open('https://allears.net/wp-content/uploads/2019/03/Lego-Anna.jpg')
+      current_user.avatar.attach(io: image, filename: 'Lego-Anna.jpg')
+    end
+    current_user.avatar
+  end
 end
