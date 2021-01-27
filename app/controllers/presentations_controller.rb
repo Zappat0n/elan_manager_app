dc24a81b6245c82beb94995ea082c2dc629016ae
angel <albarros1973@gmail.com>
2021-01-27 14:24:41 +0100

    Add reference to presentations in users

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -1,5 +1,6 @@
 class PresentationsController < ApplicationController
   def index
+    @presentations = current_user.presentations.all
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-  has_many :presentations
+  has_many :presentations_external, -> { where('group_id IS NULL') }, class_name: 'Presentation', foreign_key: 'author_id'
+  has_many :external_presentations, through: :presentations_external, source: :author
+
+  has_many :presentations, -> { where('group_id IS NOT NULL') }, foreign_key: 'author_id'
+
+  has_many :groups, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :rememberable, authentication_keys: [:name]
