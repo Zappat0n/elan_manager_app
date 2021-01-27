c5fb0f7b5fc0d73090269413de5d82f5870c4e77
angel <albarros1973@gmail.com>
2021-01-27 11:57:37 +0100

    Create presentation controller

---
+++ app/controllers/presentations_controller.rb
@@ -0,0 +1,7 @@
+class PresentationsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'presentations/index'
+  get 'presentations/new'
   root 'static_pages#index'
 
   devise_for :users, controllers: { registrations: 'registrations' }
