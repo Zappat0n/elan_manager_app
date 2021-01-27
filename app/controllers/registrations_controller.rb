d65b8be2d71ad9add1ab19016e6ef2538c291203
angel <albarros1973@gmail.com>
2021-01-27 07:16:28 +0100

    Fix problem with ActiveStorage

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,10 +2,11 @@ class RegistrationsController < ApplicationController
   include RegistrationsHelper
 
   def update
-    current_user.avatar.attach(params[:avatar])
+    current_user.avatar.attach(params['/users'][:avatar])
     if current_user.avatar.attached?
       redirect_to root_path, notice: 'Avatar updated'
     else
+      debugger
       redirect_to root_path, alert: 'Error updating avatar'
     end
   end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @user = current_user
+  end
 end
