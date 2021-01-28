deac486498266f567afe7d56dd531bf363a5d4e9
angel <albarros1973@gmail.com>
2021-01-28 10:04:33 +0100

    Create styles for my-card

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -2,6 +2,6 @@ class Presentation < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :group, optional: true
 
-  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   validates :amount, presence: true
 end
