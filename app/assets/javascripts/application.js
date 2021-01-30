830f51cfc5755d2b7a6870260b471491b2c0e032
angel <albarros1973@gmail.com>
2021-01-30 18:16:49 +0100

    Add font url to application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,2 @@
 //= require jquery
 //= require bootstrap-sprockets
-
-import "bootstrap";
-
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-  $('[data-toggle="popover"]').popover()
-})
