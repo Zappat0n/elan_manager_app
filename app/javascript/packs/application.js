c103c7a503edd53634bee116b75b2d2733cac9d6
angel <albarros1973@gmail.com>
2021-01-30 17:51:15 +0100

    Fix link in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "../stylesheets/application"
+import "../../assets/javascripts/application"
 
 
 Rails.start()
