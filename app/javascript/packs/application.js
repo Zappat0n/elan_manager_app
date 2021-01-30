66af287823ddef5b9c59ff49cec187dc1889aff3
angel <albarros1973@gmail.com>
2021-01-30 17:44:16 +0100

    Fix heroku deployment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "../../assets/stylesheets/application"
+import "../stylesheets/application"
 
 
 Rails.start()
