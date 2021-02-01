de0cdf0b3e5e06636d9946801e8a1cd84a00b79b
angel <albarros1973@gmail.com>
2021-02-01 10:13:57 +0100

    Add sign_out request test

--- spec/features/presentations_create_spec.rb
+++ spec/features/presentations_create_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "PresentationsCreates", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'Peter', email: 'x@x.com', password: '123456')
+    sign_in users_sign_in_path
+  end
+
+  it 'user can create an external presentation' do
+    visit new_presentation_path
+
+    fill_in "name",	with: "Multiplication"
+    click_button 'Create'
+    expect(current_path).to eq(presentations_path)
+    #expect(page).to have_text('Signed in successfully.')
+  end
 end
---
+++ spec/requests/sign_outs_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "SignOuts", type: :request do
+  before :each do
+    @user = User.create(name: 'Peter', email: 'x@x.com', password: '123456')
+    post users_sign_in_path, params: {id: @user.id }
+  end
+
+  it 'user can logout and it is redirected' do
+    delete '/users/sign_out'
+    expect(response).to redirect_to(root_path)
+    follow_redirect!
+
+    expect(response.body).to include('Signed out successfully.')
+  end
+end
