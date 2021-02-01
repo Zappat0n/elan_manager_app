4dbca2062803225836b1a52cda42176fa3e1ebc6
angel <albarros1973@gmail.com>
2021-02-01 09:57:17 +0100

    Add sign_up feature test

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,59 +3,27 @@ require 'rails_helper'
 RSpec.feature "SignUps", type: :feature do
   before :each do
     @user = User.new(name: 'Peter', email: 'x@x.com', password: '123456')
-    visit new_user_registration_path
+    visit users_sign_up_path
   end
 
   it 'user can Sign up and it is redirected' do
-    fill_in 'Name',	with: @user.name
-    fill_in 'Email',	with: @user.email
-    fill_in 'Password',	with: @user.password
-    fill_in 'Password confirmation',	with: @user.password
+    page.find_by_id('sign_up_name').fill_in with: @user.name
     click_button 'Sign up'
     expect(current_path).to eq(root_path)
     expect(page).to have_text('Welcome! You have signed up successfully.')
   end
 
   it 'name can not be empty' do
-    fill_in 'Email',	with: @user.email
-    fill_in 'Password',	with: @user.password
-    fill_in 'Password confirmation',	with: @user.password
     click_button 'Sign up'
+    expect(current_path).to eq(users_path)
     expect(page).to have_text("Name can't be blank")
   end
 
-  it 'email can not be empty' do
-    fill_in 'Name',	with: @user.name
-    fill_in 'Password',	with: @user.password
-    fill_in 'Password confirmation',	with: @user.password
-    click_button 'Sign up'
-    expect(page).to have_text("Email can't be blank")
-  end
-
-  it 'password can not be empty' do
-    fill_in 'Name',	with: @user.name
-    fill_in 'Email',	with: @user.email
-    fill_in 'Password confirmation',	with: @user.password
-    click_button 'Sign up'
-    expect(page).to have_text("Password can't be blank")
-  end
-
-  it 'password and password confirmation should be equal' do
-    fill_in 'Name',	with: @user.name
-    fill_in 'Email',	with: @user.email
-    fill_in 'Password',	with: @user.password
-    fill_in 'Password confirmation',	with: ' '
-    click_button 'Sign up'
-    expect(page).to have_text("Password confirmation doesn't match Password")
-  end
-
-  it 'email must be unique' do
+  it 'name must be unique' do
     @user2 = User.create(name: 'Peter', email: 'x@x.com', password: '123456')
 
-    fill_in 'Name',	with: @user.name
-    fill_in 'Email',	with: @user.email
-    fill_in 'Password',	with: @user.password
-    fill_in 'Password confirmation',	with: @user.password
+    page.find_by_id('sign_up_name').fill_in with: @user.name
     click_button 'Sign up'
-    expect(page).to have_text('Email has already been taken')
-  endend
+    expect(page).to have_text('Name has already been taken')
+  end
+end
