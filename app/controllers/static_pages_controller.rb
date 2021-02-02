cfd77de0c533244592cbbb9aeafceecec8b9a184
angel <albarros1973@gmail.com>
2021-02-02 10:16:41 +0100

    Change Static Pages index and views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,10 @@ class StaticPagesController < ApplicationController
   def index
     if current_user
       @title = current_user.name.upcase
-      @render = 'static_pages/user_show'
+      render 'static_pages/user_show'
     else
       @title = 'ELAN MONTESSORI'
-      @render = 'static_pages/index'
+      render 'static_pages/index'
     end
   end
 end
