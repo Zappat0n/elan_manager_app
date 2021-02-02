80a520d5db45efead8fe56e36719d135bc18666a
angel <albarros1973@gmail.com>
2021-02-02 10:26:46 +0100

    Remove login from presentation index

--- app/helpers/presentations_helper.rb
+++ app/helpers/presentations_helper.rb
@@ -2,4 +2,18 @@ module PresentationsHelper
   def presentation_params
     params.require(:presentation).permit(:name, :group_id, :author_id, :amount)
   end
+
+  def icon_or_name(presentation)
+    return unless presentation.group
+
+    if presentation.group.avatar.attached?
+      # <div class="image">
+      image_tag presentation.group.avatar.variant(resize_to_limit: [75,75])
+      # </div>
+    else
+      # <div class = "my-title ml-5">
+      presentation.group.name
+      # </div>
+    end
+  end
 end
